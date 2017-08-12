349dae55702ff696e51e056da9941f84f03d7179
Dominic Geargeoura <dom.geargeoura@rga.com>
2017-08-12 16:15:50 +0100

    lots of style/content tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -143,7 +143,7 @@ gulp.task('watch', (cb) => {
       port: config.defaultPort,
       server: config.paths.dist
   });
-  gulp.watch(config.paths.srcStyles + '/**/*.js',['scripts']);
+  gulp.watch(config.paths.srcScripts + '/**/*.js',['scripts']);
   gulp.watch(config.paths.srcStyles + '/**/*.scss',['styles']);
   gulp.watch([config.paths.templates + '/**/*.pug', config.paths.data + '/**/*.yaml'],['templates']);
   gulp.watch(config.paths.srcImages + '/**/*',['images']);
